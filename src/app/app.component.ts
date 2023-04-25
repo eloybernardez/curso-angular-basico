68f5f14eb0b0a24d756f5049209c7073780adb21
eloy <eloy.ricardo.550@gmail.com>
2023-04-25 19:14:52 -0300

    [ADD] initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Eloy';
+  age = 29;
+  img = 'https://cataas.com/cat';
+  btnDisabled = true;
+  person = {
+    name: 'Eloy',
+    age: 19,
+    avatar: 'https://cataas.com/cat',
+  };
 }
---
+++ src/app/recap.ts
@@ -0,0 +1,20 @@
+import { last } from 'rxjs';
+
+const username = 'eloyb';
+
+export const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+sum(4, 13);
+
+class Person {
+  constructor(private age: number, lastName: string) {}
+
+  get getAge(): number {
+    return this.age;
+  }
+}
+
+const eloy = new Person(29, 'Bernardez');
+console.log(eloy.getAge);
